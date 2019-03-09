e074ad2b23662ea71a4ca2d6f0a4ac90a45dbce1
Maksym Dukov <maxopowerful@gmail.com>
2019-03-09 22:33:47 +0200

    Updates

---
+++ precache-manifest.49acbe770373bbbb83aaa24893f27da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a902862d6c104f1419b",
+    "url": "/simple_chat/static/js/runtime~main.3a70c55a.js"
+  },
+  {
+    "revision": "38b8acb9562fd7958330",
+    "url": "/simple_chat/static/js/main.2919804b.chunk.js"
+  },
+  {
+    "revision": "25324cc26179a8ff5520",
+    "url": "/simple_chat/static/js/2.dc597446.chunk.js"
+  },
+  {
+    "revision": "38b8acb9562fd7958330",
+    "url": "/simple_chat/static/css/main.be17816a.chunk.css"
+  },
+  {
+    "revision": "3e7b1a25029872436dfe29036540058b",
+    "url": "/simple_chat/index.html"
+  }
+];
